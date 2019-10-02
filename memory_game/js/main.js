3c41120773bef363102abbe9d2b26a2faa83d78a
Ken Chow <kenchow@Chows-Macbook-Pro.local>
2019-10-02 17:56:13 +0100

    tidied up game logic and code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,26 @@
 console.log("Up and running!");
-const cards = ["queen", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[3];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else if (cardsInPlay.length === 2) {
+  console.log("Sorry, try again.");
 }
-	else {
-	alert("Sorry, try again.");
 }
+
+function flipcard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+flipcard(0);
+flipcard(2);
+
+
+
+
+
 
 
 
