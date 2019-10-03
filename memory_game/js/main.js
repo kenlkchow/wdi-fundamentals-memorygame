bb0c0c15419acb002271b1bfa981d80cd1871324
Ken Chow <kenchow@Chows-Macbook-Pro.local>
2019-10-03 01:18:45 +0100

    implemented objects with image path suits

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,46 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{ 
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	}
+];
 var cardsInPlay = [];
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
 } else if (cardsInPlay.length === 2) {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
 
 function flipcard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit)
+
 };
+
 flipcard(0);
 flipcard(2);
-
-
 
 
 
